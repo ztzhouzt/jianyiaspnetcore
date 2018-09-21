c35ec3c264fd3992f31ffdec965a330c13a53b69
zhou <1249126625@qq.com>
2018-09-21 11:20:10 +0800

    第一次提交

---
+++ jianyiaspnetcore/Models/TodoItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jianyiaspnetcore.Models
+{
+    /// <summary>
+    /// 待办事项
+    /// </summary>
+    public class TodoItem
+    {
+        /// <summary>
+        /// 标识
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 该事项是否已完成
+        /// </summary>
+        public bool IsDon { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 截止时间
+        /// </summary>
+        public DateTimeOffset? DueAt { get; set; }
+    }
+}
---
+++ jianyiaspnetcore/Models/TodoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jianyiaspnetcore.Models
+{
+    public class TodoViewModel
+    {
+        public TodoItem[] items { get; set; }
+    }
+}
